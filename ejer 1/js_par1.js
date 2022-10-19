a7f9c1603ba60deba612cf26ca7c0b8f3d74940e
CATALINALIZETH <114926059+CATALINALIZETH@users.noreply.github.com>
2022-10-19 11:59:10 -0500

    Add files via upload

---
+++ ejer 1/js_par1.js
@@ -0,0 +1,14 @@
+let num1,lee,num2,lee2;
+
+num1 = window.prompt("Ingresa primer numero: ");
+lee = parseInt(num1);
+num2 = window.prompt("Ingresa segundo numero: ");
+lee2 = parseInt(num2);
+
+if(num1>num2){
+    document.write(`Este numero es mayor ${num1}`);
+}
+
+if(num1<num2){
+    document.write(`Este numero es mayor ${num1}`);
+}
